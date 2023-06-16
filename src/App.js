995533d048ef617e1f619d723dd14f2852a4a3a3
Jomar Banting <shadowlover.05@gmail.com>
2023-06-15 22:50:42 -0400

    4 passed out of 10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import Homepage from "./homepage";
+import Order from "./pizza-form";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <nav>
+        <Link to="/">Homepage</Link>
+        <Link id="order-pizza" to="/pizza">Order Pizza!</Link>
+      </nav>
+      <h1>Pizza App</h1>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/pizza" element={<Order/>} />
+      </Routes>
+    </div>
   );
 };
 export default App;
